3fac9b98bb0208615e8176d640d1ce367d058136
Jan Čepelák <jan.cepelak@datamole.cz>
2020-10-06 16:49:55 +0200

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
 import './App.css';
+import Navbar from './app/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className={"App"}>
+        <Switch>
+          <Route
+            exact
+            path={"/"}
+            render={() => (
+              <>
+                Hello
+              </>
+            )}
+          />
+          <Redirect to={"/"} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/app/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar: React.FC = () => (
+    <nav className={"Navbar"}>
+        Hello
+    </nav>
+);
+
+export default Navbar;
